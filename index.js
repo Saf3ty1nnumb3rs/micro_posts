678b4f5bd0f93cb132d1db79e16085fb93ca18e8
Joshua Sample <josh.sample@ware2go.com>
2023-10-03 20:40:58 -0400

    setup events post

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
@@ -11,16 +12,31 @@ const APP_PORT = 4000;
 // no db for toy app
 const posts = {};
 
+// POSTS
 app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
 
   posts[id] = { id, title };
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: { id, title },
+  }).catch((err) => {
+    console.log(err.message);
+  });
   res.status(201).send(posts[id]);
+});
+
+// EVENTS
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type);
+
+  res.send({});
 });
 
 app.listen(APP_PORT, () => {
